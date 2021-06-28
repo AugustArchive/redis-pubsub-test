9429e71df582bd49ebc157569b8a917f276c6ef3
Noel <cutie@floofy.dev>
2021-06-27 23:06:42 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# redis pubsub test
+something i am doing for the lols, might use in the future for microservices, who knows i wish i fucking knew
+
+no this isn't supposed to be serious as of this readme, i dont care if i swear :)
---
+++ redis-pubsub-test/Bootstrap.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace redis_pubsub_test
+{
+    class Bootstrap
+    {
+        private readonly Process process = Process.GetCurrentProcess();
+        private IServiceProvider _provider;
+        private IConfigurationRoot root;
+
+        public Bootstrap()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddYamlFile("config.yml");
+
+            root = builder.Build();
+        }
+
+        public async Task StartAsync()
+        {
+            ConfigureProcessEvents();
+            var services = new ServiceCollection();
+            ConfigureAllServices(services);
+
+            var provider = services.BuildServiceProvider();
+            _provider = provider;
+
+            await Task.Delay(-1);
+        }
+
+        private void Process_OnExited(object sender, EventArgs args)
+        {
+            // TODO: this
+        }
+
+        private void ConfigureProcessEvents()
+        {
+            process.Exited += Process_OnExited;
+        }
+
+        private void ConfigureAllServices(IServiceCollection services)
+        {
+            // TODO: this
+        }
+    }
+}
